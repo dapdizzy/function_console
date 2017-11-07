7e8f008ae2d08620266b989329351c1f5a39732d
pyatkov <pyatkov@media-saturn.com>
2017-11-07 16:54:48 +0300

    Made teh app Heroku ready: one more time

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,8 @@ use Mix.Config
 config :function_console, FunctionConsoleWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "fierce-scrubland-26538.herokuapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
