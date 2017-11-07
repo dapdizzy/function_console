2298cc48cf7f4a875168ca159dd1b0b998ea0382
pyatkov <pyatkov@media-saturn.com>
2017-11-07 16:50:52 +0300

    Made teh app Heroku ready

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :function_console, FunctionConsoleWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "fierce-scrubland-26538.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
